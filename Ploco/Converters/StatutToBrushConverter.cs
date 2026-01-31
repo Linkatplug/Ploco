cefb2bef23a053da7eeee6f8f78058bf3164644c
Linkatplug <36280686+Linkatplug@users.noreply.github.com>
2026-01-31 22:32:17 +0100

    Refactor UI with tile-based locomotive layout

--- Ploco/Converters/StatutToBrushConverter.cs
+++ Ploco/Converters/StatutToBrushConverter.cs
@@ -10,9 +10,21 @@ namespace Ploco.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is StatutLocomotive statut)
+            if (value is LocomotiveStatus statut)
             {
                 return statut switch
+                {
+                    LocomotiveStatus.Ok => Brushes.Green,
+                    LocomotiveStatus.DefautMineur => Brushes.Gold,
+                    LocomotiveStatus.AControler => Brushes.Orange,
+                    LocomotiveStatus.HS => Brushes.Red,
+                    _ => Brushes.Gray,
+                };
+            }
+
+            if (value is StatutLocomotive legacyStatut)
+            {
+                return legacyStatut switch
                 {
                     StatutLocomotive.Ok => Brushes.Green,
                     StatutLocomotive.DefautMineur => Brushes.Gold,
@@ -26,7 +38,7 @@ namespace Ploco.Converters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
---
+++ Ploco/Models/AppState.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Ploco.Models
+{
+    public class AppState
+    {
+        public List<RollingStockSeries> Series { get; set; } = new();
+        public List<LocomotiveModel> Locomotives { get; set; } = new();
+        public List<TileModel> Tiles { get; set; } = new();
+    }
+}
