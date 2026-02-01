b1876efa85e19529c10cc2d17ec6c3376978bfdf
Linkatplug <36280686+Linkatplug@users.noreply.github.com>
2026-02-01 13:32:56 +0100

    Fix missing CollectionViewSource using

--- Ploco/MainWindow.xaml.cs
+++ Ploco/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using Microsoft.Win32;
